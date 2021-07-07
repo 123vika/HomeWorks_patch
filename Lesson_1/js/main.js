cb12c5cdb21aafef5021bab59a2aad44b5ad721d
Victoria <123vika@gmail.com>
2021-07-07 09:33:01 +0300

    small fixing

--- Lesson_1/js/main.js
+++ Lesson_1/js/main.js
@@ -2,7 +2,7 @@ import { sayHello } from "./greeting.js";
 import "./datecalc.js";
 import * as swicthTimer from './switchTimerDate';
 import * as timerFile from './timer.js';
-
+import "../css/style.css";
 import chunk from 'lodash/chunk';
 console.log(chunk(['a', 'b', 'c', 'd'], 2)); // [['a', 'b'], ['c', 'd']]
 
@@ -10,5 +10,5 @@ console.log(chunk(['a', 'b', 'c', 'd'], 2)); // [['a', 'b'], ['c', 'd']]
 
 
 // console.log(sayHello("Ivan"));
-import "../css/style.css";
+
 
